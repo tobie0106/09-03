e617cf42c128a1aabe9a98e6b5e134d074c2fd5d
Truong Thien Phuc <truongthienphuc6655@gmail.com>
2026-03-23 10:01:24 +0700

    bai tap 9/3

---
+++ routes/products.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var router = express.Router();
+let productModel = require('../schemas/products')
+let { ConvertTitleToSlug } = require('../utils/titleHandler')
+let { getMaxID } = require('../utils/IdHandler')
+let { checkLogin, checkRole } = require('../utils/authHandler')
+
+//getall
+router.get('/', async function (req, res, next) {
+  // let queries = req.query;
+  // let titleQ = queries.title ? queries.title : '';
+  // let minPrice = queries.minPrice ? queries.minPrice : 0;
+  // let maxPrice = queries.maxPrice ? queries.maxPrice : 1E6;
+  // let page = queries.page ? queries.page : 1;
+  // let limit = queries.limit ? queries.limit : 10;
+  // console.log(queries);
+  // let result = data.filter(
+  //   function (e) {
+  //     return (!e.isDeleted) && e.title.includes(titleQ) &&
+  //       e.price >= minPrice && e.price <= maxPrice
+  //   }
+  // );
+  // result = result.splice(limit * (page - 1), limit)
+  // res.send(result);
+  let products = await productModel.find({});
+  res.send(products)
+});
+//get by ID
+router.get('/:id', async function (req, res, next) {
+  try {
+    let result = await productModel.find({ _id: req.params.id });
+    if (result.length > 0) {
+      res.send(result)
+    } else {
+      res.status(404).send({
+        message: "id not found"
+      })
+    }
+  } catch (error) {
+    res.status(404).send({
+      message: "id not found"
+    })
+  }
+});
+
+
+router.post('/', checkLogin, checkRole('ADMIN', 'MODERATOR'), async function (req, res, next) {
+  let newItem = new productModel({
+    title: req.body.title,
+    slug: ConvertTitleToSlug(req.body.title),
+    price: req.body.price,
+    description: req.body.description,
+    category: req.body.category
+  })
+  await newItem.save()
+  res.send(newItem);
+  // let newObj = {
+  //   id: (getMaxID(data) + 1) + '',
+  //   title: req.body.title,
+  //   slug: ConvertTitleToSlug(req.body.title),
+  //   price: req.body.price,
+  //   description: req.body.description,
+  //   category: req.body.category,
+  //   images: req.body.images,
+  //   creationAt: new Date(Date.now()),
+  //   updatedAt: new Date(Date.now())
+  // }
+  // data.push(newObj);
+  // console.log(data);
+  // res.send(newObj);
+  // //console.log(g);
+
+})
+router.put('/:id', checkLogin, checkRole('ADMIN', 'MODERATOR'), async function (req, res, next) {
+  let id = req.params.id;
+  let updatedItem = await productModel.findByIdAndUpdate(
+    id, req.body, {
+    new: true
+  }
+  )
+  res.send(updatedItem)
+
+})
+router.delete('/:id', checkLogin, checkRole('ADMIN'), async function (req, res, next) {
+  let id = req.params.id;
+  let updatedItem = await productModel.findByIdAndUpdate(
+    id, {
+    isDeleted: true
+  }, {
+    new: true
+  }
+  )
+  res.send(updatedItem)
+
+})
+
+module.exports = router;
